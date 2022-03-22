e9cb3b98e25b23c67bb1755ccaa81445abd6a7af
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2022-03-22 22:01:31 +0600

    added

--- index.js
+++ index.js
@@ -1,44 +1,195 @@
+
+
+
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+
 const app = express();
 const port = process.env.PORT || 5000;
-
 
 //midleware
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xgxwj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.638jm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-client.connect(err => {
-    const servicesCollection = client.db("Hospital-Services").collection("allServices");
-    const usersCollection = client.db("Toyoya-car").collection("users");
-    
-    //get allServices
-    app.get("/allServices", async (req, res) => {
-        const result = await servicesCollection.find({}).toArray();
-        res.send(result);
-      });
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('travel_tour');
+        const travelsCollection = database.collection('travels');
+        const bookingCollection = database.collection('bookingCollection');
+        const CustomerInfo = database.collection('CustomerInfo');
 
-      app.use('/users', async (req, res) => {
-        console.log(req.body);
-        const result = await usersCollection.find({}).toArray();
-        console.log(result);
-        res.send(result);
-    })
+        // Get travels API
 
-  });
+        app.get('/travels', async (req, res) => {
+            console.log(req.body);
+            const cursor = travelsCollection.find({});
+            const travels = await cursor.toArray();
+            res.send(travels);
+        })
 
-console.log(uri);
+        // Booking travel............
+        app.post('/Addbooking', async (req, res) => {
+            console.log(req.body);
+            const result = await bookingCollection.insertOne(req.body);
+            res.send(result);
+        })
+
+
+        // geting id to show single data form travels database.... 
+        app.get('/book/:id', async (req, res) => {
+            console.log('Hitedddddd Body.....', req.body);
+        })
+
+
+
+
+        app.use('/getBooking', async (req, res) => {
+            console.log(req.body);
+            const result = await bookingCollection.find({}).toArray();
+            console.log(result);
+            res.send(result);
+        })
+
+
+
+        // Customer information insert in database.
+        app.post('/customerInfo', async (req, res) => {
+            console.log(req.body);
+            const result = await CustomerInfo.insertOne(req.body);
+            console.log(result)
+            res.send(result);
+        })
+
+        // customer info getinng from data  
+        app.use('/myOrders', async (req, res) => {
+            console.log(req.body);
+            const result = await CustomerInfo.find({}).toArray();
+            console.log(result);
+            res.send(result);
+        })
+
+
+
+        app.delete('/deleteOrdersByUser/:id', async (req, res) => {
+            const query = { _id: ObjectId(req.params.id) }
+            console.log(query);
+            const result = await CustomerInfo.deleteOne(query);
+            res.send(result);
+
+        })
+
+
+        // Add New Services To Database............
+        app.post('/addServiceDB', async (req, res) => {
+            const result = await travelsCollection.insertOne(req.body);
+            res.send(result);
+        })
+
+
+
+
+        app.put('/adminPannel/approved/:id', async (req, res) => {
+            const query = { _id: ObjectId(req.params.id) }
+            const result = await CustomerInfo.updateOne(query, {
+                $set: {
+                    status: 'Approved'
+                }
+            });
+            console.log(result);
+            res.send(result);
+        })
+
+
+
+
+    }
+
+    finally {
+        //await client.close();
+    }
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
-  res.send('Hospital services server')
+    res.send('running the server')
 })
 
 app.listen(port, () => {
-  console.log('running the server',port);
+    console.log('hitting the post', port);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// require('dotenv').config();
+// const cors = require('cors');
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+
+// //midleware
+// app.use(cors());
+// app.use(express.json());
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xgxwj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// client.connect(err => {
+//     const servicesCollection = client.db("Hospital-Services").collection("allServices");
+//     const usersCollection = client.db("Toyoya-car").collection("users");
+    
+//     //get allServices
+//     app.get("/allServices", async (req, res) => {
+//         const result = await servicesCollection.find({}).toArray();
+//         res.send(result);
+//       });
+
+//       app.use('/users', async (req, res) => {
+//         console.log(req.body);
+//         const result = await usersCollection.find({}).toArray();
+//         console.log(result);
+//         res.send(result);
+//     })
+
+//   });
+
+// console.log(uri);
+
+// app.get('/', (req, res) => {
+//   res.send('Hospital services server')
+// })
+
+// app.listen(port, () => {
+//   console.log('running the server',port);
+// })
